14e63239ed713daadc5063b224407c245e5548f1
Jagmanpreet Kaur <jagmanpreetkaur16@gmail.com>
2025-06-21 21:43:17 +0530

    Fix: export Toaster from sonner.tsx

--- src/components/ui/sonner.tsx
+++ src/components/ui/sonner.tsx
@@ -1,5 +1,6 @@
 import { useTheme } from "next-themes"
-import { Toaster as Sonner, toast } from "sonner"
+import React from "react";
+import { Toaster as Sonner } from "sonner";
 
 type ToasterProps = React.ComponentProps<typeof Sonner>
 
@@ -25,3 +26,5 @@ const Toaster = ({ ...props }: ToasterProps) => {
     />
   )
 }
+
+export { Toaster };
